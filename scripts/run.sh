b8133739a0d3c70b7c5ef614403e6d76fbbcf55f
edsonmoura321 <92758902+edsonmoura321@users.noreply.github.com>
2024-05-30 22:25:35 -0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,96 +1,23 @@
-version=$1 ##1024, 512, 256
-seed=123
 
-name=dynamicrafter_$1_mp_seed${seed}
+ckpt=checkpoints/tooncrafter_512_interp_v1/model.ckpt
+config=configs/inference_512_v1.0.yaml
 
-ckpt=checkpoints/dynamicrafter_$1_v1/model.ckpt
-config=configs/inference_$1_v1.0.yaml
-
-prompt_dir=prompts/$1/
+prompt_dir=prompts/512_interp/
 res_dir="results"
 
-if [ "$1" == "256" ]; then
-    H=256
-    FS=3  ## This model adopts frame stride=3
-elif [ "$1" == "512" ]; then
-    H=320
-    FS=24 ## This model adopts FPS=24
-elif [ "$1" == "1024" ]; then
-    H=576
-    FS=10 ## This model adopts FPS=10
-else
-    echo "Invalid input. Please enter 256, 512, or 1024."
-    exit 1
-fi
-
-# if [ "$1" == "256" ]; then
-# CUDA_VISIBLE_DEVICES=2 python3 scripts/evaluation/inference.py \
-# --seed 123 \
-# --ckpt_path $ckpt \
-# --config $config \
-# --savedir $res_dir/$name \
-# --n_samples 1 \
-# --bs 1 --height ${H} --width $1 \
-# --unconditional_guidance_scale 7.5 \
-# --ddim_steps 50 \
-# --ddim_eta 1.0 \
-# --prompt_dir $prompt_dir \
-# --text_input \
-# --video_length 16 \
-# --frame_stride ${FS}
-# else
-# CUDA_VISIBLE_DEVICES=2 python3 scripts/evaluation/inference.py \
-# --seed 123 \
-# --ckpt_path $ckpt \
-# --config $config \
-# --savedir $res_dir/$name \
-# --n_samples 1 \
-# --bs 1 --height ${H} --width $1 \
-# --unconditional_guidance_scale 7.5 \
-# --ddim_steps 50 \
-# --ddim_eta 1.0 \
-# --prompt_dir $prompt_dir \
-# --text_input \
-# --video_length 16 \
-# --frame_stride ${FS} \
-# --timestep_spacing 'uniform_trailing' --guidance_rescale 0.7
-# fi
+FS=10 ## This model adopts FPS=5, range recommended: 5-30 (smaller value -> larger motion)
 
 
-## multi-cond CFG: the <unconditional_guidance_scale> is s_txt, <cfg_img> is s_img
-#--multiple_cond_cfg --cfg_img 7.5
-#--loop
 
-## inference using single node with multi-GPUs:
-if [ "$1" == "256" ]; then
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch \
---nproc_per_node=8 --nnodes=1 --master_addr=127.0.0.1 --master_port=23456 --node_rank=0 \
-scripts/evaluation/ddp_wrapper.py \
---module 'inference' \
+seed=123
+name=tooncrafter_512_interp_seed${seed}
+CUDA_VISIBLE_DEVICES=1 python3 scripts/evaluation/inference.py \
 --seed ${seed} \
 --ckpt_path $ckpt \
 --config $config \
 --savedir $res_dir/$name \
 --n_samples 1 \
---bs 1 --height ${H} --width $1 \
---unconditional_guidance_scale 7.5 \
---ddim_steps 50 \
---ddim_eta 1.0 \
---prompt_dir $prompt_dir \
---text_input \
---video_length 16 \
---frame_stride ${FS}
-else
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch \
---nproc_per_node=8 --nnodes=1 --master_addr=127.0.0.1 --master_port=23456 --node_rank=0 \
-scripts/evaluation/ddp_wrapper.py \
---module 'inference' \
---seed ${seed} \
---ckpt_path $ckpt \
---config $config \
---savedir $res_dir/$name \
---n_samples 1 \
---bs 1 --height ${H} --width $1 \
+--bs 1 --height 320 --width 512 \
 --unconditional_guidance_scale 7.5 \
 --ddim_steps 50 \
 --ddim_eta 1.0 \
@@ -98,5 +25,4 @@ scripts/evaluation/ddp_wrapper.py \
 --text_input \
 --video_length 16 \
 --frame_stride ${FS} \
---timestep_spacing 'uniform_trailing' --guidance_rescale 0.7 --perframe_ae
-fi
+--timestep_spacing 'uniform_trailing' --guidance_rescale 0.7 --perframe_ae --interp
